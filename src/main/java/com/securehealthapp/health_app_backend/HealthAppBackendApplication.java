04da0446db73c458516b37c422f6846f96702b36
Louise Deeth <louisedeeth@gmail.com>
2025-11-18 18:13:32 +0000

    Initial backend project

---
+++ src/main/java/com/securehealthapp/health_app_backend/HealthAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.securehealthapp.health_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/securehealthapp/health_app_backend/HealthAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securehealthapp.health_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
